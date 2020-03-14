e18f3aeabbbe2787e1a586ef8605276212dcd6f6
ibrahimhassanin <ibrahim.hassanin@intellisc.com>
2020-03-14 17:01:32 +0200

    Making feign Client in gateway has operation to get UserDetails that can retrieve the current user login

--- quiz-master-service/src/main/java/com/isoft/qzm/feign/GatewayProxy.java
+++ quiz-master-service/src/main/java/com/isoft/qzm/feign/GatewayProxy.java
@@ -2,12 +2,13 @@ package com.isoft.qzm.feign;
 
 import com.isoft.qzm.client.AuthorizedFeignClient;
 import com.isoft.qzm.service.dto.UserDTO;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @AuthorizedFeignClient(name="gateway")
+@RibbonClient(name="quizMasterService")
 public interface GatewayProxy {
-
     @GetMapping("services/uaa/api/account/{userId}")
-    UserDTO getUserDetails(@PathVariable("userId")  String userId);
+    UserDTO getAccount(@PathVariable("userId") String userId);
 }
